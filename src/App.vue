aedd93f2c801d5de8c04b4d1c22cec910f63f1c9
rohxnsxngh <98181667+rohxnsxngh@users.noreply.github.com>
2022-11-21 21:10:07 -0600

    checking deployment features

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,85 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div id="loader" v-if="isLoading"></div>
+  <div class="nav-container" v-if="!isLoading">
+    <nav id="navbar">
+      <div>
+        <a href="https://github.com/rohxnsxngh">
+          <div class="link-container">
+            <img
+              src="./assets/github.png"
+              style="width: 35px; height: 35px; margin-bottom: 15px"
+            /><img />
+          </div>
+        </a>
+        <a href="https://www.linkedin.com/in/rohan-singh1122/">
+          <div class="link-container">
+            <img
+              src="./assets/linkedin.png"
+              style="width: 35px; height: 35px; margin-bottom: 15px"
+            /><img />
+          </div>
+        </a>
+        <a href="mailto:rohan.singh.do@gmail.com">
+          <div class="link-container">
+            <img
+              src="./assets/email.png"
+              style="width: 35px; height: 35px; margin-bottom: 15px"
+            /><img />
+          </div>
+        </a>
+        <a href="https://www.instagram.com/rohxnsxngh/">
+          <div class="link-container">
+            <img
+              src="./assets/instagram.png"
+              style="width: 35px; height: 35px; margin-bottom: 15px"
+            /><img />
+          </div>
+        </a>
+      </div>
+    </nav>
+  </div>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+export default {
+  name: "main",
+  components: {},
+  data() {
+    return {
+      isLoading: false,
+    };
+  },
+  methods: {
+    checkLoading() {
+      var timeleft = 5;
+      var downloadTimer = setInterval(function () {
+        if (timeleft <= 0) {
+          clearInterval(downloadTimer);
+          this.isLoading = false;
+        }
+        timeleft -= 1;
+      }, 1000);
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+#navbar {
+  display: flex;
+  background-color: transparent;
+  transform: translateY(-100%);
+  transition: transform 0.5s cubic-bezier(0.4, 0, 0.2, 1);
+}
+#navbar a {
+  color: rgb(255, 255, 255);
+  text-decoration: none;
+}
+#navbar > * {
+  padding: 1rem;
+}
+.nav-container:hover #navbar {
+  transform: translateY(0);
 }
 </style>
---
+++ src/index.js
@@ -0,0 +1,203 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { FirstPersonControls } from "three/examples/jsm/controls/FirstPersonControls.js";
+import { Water } from "three/examples/jsm/objects/Water.js";
+import { Sky } from "three/examples/jsm/objects/Sky.js";
+
+let container;
+let camera, scene, renderer, clock, composer;
+let controls, water, upperwater, sun;
+let pointLight, ambientLight;
+let time = 0;
+
+function init() {
+  container = document.getElementById("container");
+
+  // Scene & Camera
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(
+    55,
+    window.innerWidth / window.innerHeight,
+    1,
+    20000
+  );
+  camera.rotateOnAxis(new THREE.Vector3(0, 1, 0), -Math.PI / 4);
+  camera.position.set(-4000, 30, 4000);
+
+  //fog
+  const color = 0xeb4950; // change color
+  const near = 100;
+  const far = 1000;
+  scene.fog = new THREE.Fog(color, near, far);
+
+  // Renderer
+  renderer = new THREE.WebGLRenderer({
+    antialias: true,
+    alpha: true,
+    powerPreference: "high-performance",
+  });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  renderer.toneMapping = THREE.ACESFilmicToneMapping;
+  container.appendChild(renderer.domElement);
+
+  //LIGHT
+  pointLight = new THREE.PointLight(0xeb4950);
+  pointLight.position.set(-4000, 0, 4100);
+  scene.add(pointLight);
+
+  ambientLight = new THREE.AmbientLight(0x080808);
+  scene.add(ambientLight);
+
+  // White directional light at half intensity shining from the top.
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 15);
+  scene.add(directionalLight);
+
+  //clock
+  clock = new THREE.Clock();
+
+  //Sun
+  sun = new THREE.Vector3();
+
+  //Water
+  const waterGeometry = new THREE.PlaneGeometry(20000, 20000);
+
+  water = new Water(waterGeometry, {
+    textureWidth: 512,
+    textureHeight: 512,
+    waterNormals: new THREE.TextureLoader().load(
+      "../src/assets/waternormals.jpg",
+      function (texture) {
+        texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+      }
+    ),
+    sunDirection: new THREE.Vector3(),
+    sunColor: 0xe27d60,
+    waterColor: 0x6aeff5,
+    distortionScale: 3.7,
+    fog: scene.fog !== undefined,
+  });
+  water.rotation.x = -Math.PI / 2;
+  scene.add(water);
+
+  upperwater = new Water(waterGeometry, {
+    textureWidth: 512,
+    textureHeight: 512,
+    waterNormals: new THREE.TextureLoader().load(
+      "/waternormals.jpg",
+      function (texture) {
+        texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+      }
+    ),
+    sunDirection: new THREE.Vector3(),
+    sunColor: 0xe27d60,
+    waterColor: 0xc38d9e,
+    distortionScale: 3.7,
+    fog: scene.fog !== undefined,
+  });
+  upperwater.rotation.x = -Math.PI / 2;
+  upperwater.position.set(0, 100, 0);
+  upperwater.rotateOnAxis(new THREE.Vector3(0, 1, 0), Math.PI);
+  scene.add(upperwater);
+
+  //SkyBox
+  const sky = new Sky();
+  sky.scale.setScalar(20000);
+  scene.add(sky);
+
+  const skyUniforms = sky.material.uniforms;
+
+  skyUniforms["turbidity"].value = 0.1;
+  skyUniforms["rayleigh"].value = 4; // twilight mode is 0, sunset mode is 3
+  skyUniforms["mieCoefficient"].value = 0.5;
+  skyUniforms["mieDirectionalG"].value = 0.7;
+
+  const parameters = {
+    elevation: 0,
+    azimuth: 135,
+  };
+
+  const pmremGenerator = new THREE.PMREMGenerator(renderer);
+  let renderTarget;
+
+  function updateSun() {
+    const phi = THREE.MathUtils.degToRad(90 - parameters.elevation);
+    const theta = THREE.MathUtils.degToRad(parameters.azimuth);
+    sun.setFromSphericalCoords(1, phi, theta);
+    sky.material.uniforms["sunPosition"].value.copy(sun);
+    water.material.uniforms["sunDirection"].value.copy(sun).normalize();
+    if (renderTarget !== undefined) renderTarget.dispose();
+    renderTarget = pmremGenerator.fromScene(sky);
+    scene.environment = renderTarget.texture;
+  }
+
+  updateSun();
+
+  const axesHelper = new THREE.AxesHelper(5);
+  scene.add(axesHelper);
+
+  //curve
+//   createCurve(scene);
+//   homePage(scene);
+//   aboutPage(scene);
+//   expPage(scene);
+//   contactPage(scene);
+//   createKeys(scene);
+//   createWelcome(scene);
+//   createAmbientSound(camera);
+//   createBackground(scene); // pretty detailed background
+
+  //Controls
+  //First Person Controls
+  controls = new FirstPersonControls(camera, renderer.domElement);
+  controls.movementSpeed = 300;
+  controls.lookSpeed = 0.125;
+  controls.heightMin = 10;
+  controls.heightCoef = 10;
+  controls.constrainVertical = true;
+  controls.mouseDragOn = false;
+  //controls mouse look around
+  controls.activeLook = true;
+  controls.lookVertical = false;
+  window.addEventListener("resize", onWindowResize);
+}
+
+//Fit to Window
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  // composer.setSize(window.innerWidth, window.innerHeight);
+}
+
+//Animate
+function animate(keys) {
+  setTimeout(function () {
+    requestAnimationFrame(animate);
+  }, 1000 / 500);
+  render();
+}
+
+//Render
+function render() {
+  const timer = performance.now() * 0.0025;
+  water.material.uniforms["time"].value += 1.0 / 60.0;
+  upperwater.material.uniforms["time"].value += 1.0 / 60.0;
+
+  const delta = clock.getDelta();
+
+  time += delta * 1.0 * 0.5;
+
+  controls.update(clock.getDelta());
+  renderer.render(scene, camera);
+}
+
+init();
+animate();
+
+console.log("Scene Polycount:", renderer.info.render.triangles);
+console.log("Active Drawcalls:", renderer.info.render.calls);
+console.log("Textures in Memory", renderer.info.memory.textures);
+console.log("Geometries in Memory", renderer.info.memory.geometries);
+
