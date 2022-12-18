f116e51778d69ed30eed3e786824bcbbe2a7254a
rohxnsxngh <98181667+rohxnsxngh@users.noreply.github.com>
2022-12-17 20:32:52 -0600

    second world is possible

--- src/index.js
+++ src/index.js
@@ -1,62 +1,56 @@
-import {
-    BoxBufferGeometry,
-    Color,
-    Mesh,
-    MeshBasicMaterial,
-    PerspectiveCamera,
-    Scene,
-    WebGLRenderer,
-  } from 'three';
-  
-  // Get a reference to the container element that will hold our scene
-  const container = document.querySelector('#container2');
-  
-  // create a Scene
-  const scene = new Scene();
-  
-  // Set the background color
-  scene.background = new Color('skyblue');
-  
-  // Create a camera
-  const fov = 35; // AKA Field of View
-  const aspect = container.clientWidth / container.clientHeight;
-  const near = 0.1; // the near clipping plane
-  const far = 100; // the far clipping plane
-  
-  const camera = new PerspectiveCamera(fov, aspect, near, far);
-  
-  // every object is initially created at ( 0, 0, 0 )
-  // move the camera back so we can view the scene
-  camera.position.set(0, 0, 10);
-  
-  // create a geometry
-  const geometry = new BoxBufferGeometry(2, 2, 2);
-  
-  // create a default (white) Basic material
-  const material = new MeshBasicMaterial();
-  
-  // create a Mesh containing the geometry and material
-  const cube = new Mesh(geometry, material);
-  
-  // add the mesh to the scene
-  scene.add(cube);
-  
-  // create the renderer
-  const renderer = new WebGLRenderer();
-  
-  // next, set the renderer to the same size as our container element
-  renderer.setSize(container.clientWidth, container.clientHeight);
-  
-  // finally, set the pixel ratio so that our scene will look good on HiDPI displays
-  renderer.setPixelRatio(window.devicePixelRatio);
-  
-  // add the automatically created <canvas> element to the page
-  container.append(renderer.domElement);
-  
-  // render, or 'create a still image', of the scene
-  renderer.render(scene, camera);
+import * as THREE from "three";
 
-  console.log("Scene Polycount:", renderer.info.render.triangles);
-  console.log("Active Drawcalls:", renderer.info.render.calls);
-  console.log("Textures in Memory", renderer.info.memory.textures);
-  console.log("Geometries in Memory", renderer.info.memory.geometries);
+let camera, scene, renderer, container;
+let mesh;
+
+init();
+animate();
+
+function init() {
+  container = document.getElementById("container2");
+  camera = new THREE.PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    1,
+    1000
+  );
+  camera.position.z = 400;
+
+  scene = new THREE.Scene();
+
+  const geometry = new THREE.BoxGeometry(200, 200, 200);
+  const material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+
+  mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  container.appendChild(renderer.domElement);
+
+  //
+
+  window.addEventListener("resize", onWindowResize);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  mesh.rotation.x += 0.005;
+  mesh.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+console.log("Scene Polycount:", renderer.info.render.triangles);
+console.log("Active Drawcalls:", renderer.info.render.calls);
+console.log("Textures in Memory", renderer.info.memory.textures);
+console.log("Geometries in Memory", renderer.info.memory.geometries);
